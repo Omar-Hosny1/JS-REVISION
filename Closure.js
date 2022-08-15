9810c7344bf3012d91c61909e90430294e9f6c2b
Omar Hosny <honsyomar022@gmail.com>
2022-08-15 07:41:56 +0200

    Clouser Proplem Solved

--- Closure.js
+++ Closure.js
@@ -1,5 +1,10 @@
+const addTo = (number) => {
+  return function addTwoNums(other) {
+    return number + other;
+  };
+};
 const addToThree = addTo(3);
 const addToOne = addTo(1);
 
-console.log(addToThree);
-console.log(addToOne);
+console.log(addToThree(1)); // 4
+console.log(addToOne(5)); // 6
